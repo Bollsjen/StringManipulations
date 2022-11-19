1d583aa8a1f61b66ceccf1cdc168c52cdb88bcf9
Bollsjen <55839056+Bollsjen@users.noreply.github.com>
2022-11-19 16:20:12 +0100

    comments

--- lib/components/manipulation_button.dart
+++ lib/components/manipulation_button.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
+// Generel knap til klasse
 class ManipulationButton extends StatelessWidget {
   final String title;
   final color;
   final function;
 
+  // Constructor
   const ManipulationButton(
       {
         Key? key,
@@ -13,24 +15,40 @@ class ManipulationButton extends StatelessWidget {
         required this.function
       }) : super(key: key);
 
+  // UI del
   @override
   Widget build(BuildContext context){
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 12),
       child: Container(
         decoration: BoxDecoration(
+          // Bagrundsfarve
             color: color
         ),
+        // Knappen
         child: TextButton(
+
+          // Knap trykke event
           onPressed: () {
+
+            // Metode kald
             function();
           },
+          // Centrering af tekst elementet
           child: Center(
+            // Teksten elementet
               child: Text(
+                // Teksten
                   title,
+                  // Tekst elementet
                   style: TextStyle(
+                    // Tekst farve
                       color: Colors.white,
+
+                      // Tekst vægt
                       fontWeight: FontWeight.bold,
+
+                      // Tekst størrelse
                       fontSize: 18
                   )
               )
--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -31,23 +31,34 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             controller: textController,
           ),
+
+          // Knap til lower case
           ManipulationButton(title: 'LOWER CASE', color: Colors.blue[400], function: Lowercase),
+
+          // Knap til upper case
           ManipulationButton(title: 'UPPER CASE', color: Colors.blue[400], function: Uppercase),
+
+          // Knap til capitalize
           ManipulationButton(title: 'CAPITALIZE', color: Colors.blue[400], function: Capitalize),
+
+          // Knap til reverse
           ManipulationButton(title: 'REVERSE', color: Colors.blue[400], function: Reverse)
         ],
       ),
     );
   }
 
+  // Metode til lower case
   void Lowercase(){
     textController.text = textController.text.toLowerCase();
   }
 
+  // Metode til upper case
   void Uppercase(){
     textController.text = textController.text.toUpperCase();
   }
 
+  // Metode til capitalize
   void Capitalize(){
     String text = textController.text;
     String firstChar = text[0].toUpperCase();
@@ -56,6 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
     textController.text = text;
   }
 
+  // Metode til reverse
   void Reverse(){
     textController.text = textController.text.split('').reversed.join();
   }
